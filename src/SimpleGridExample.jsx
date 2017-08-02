64fc0512d92326e3df0530cb535435d007abb924
Jinto Jose <jintoppy@gmail.com>
2017-08-02 14:18:58 +0530

    grid taking time to size columns to fit

--- src/SimpleGridExample.jsx
+++ src/SimpleGridExample.jsx
@@ -1,15 +1,17 @@
-import React, {Component} from "react";
-import {AgGridReact} from "ag-grid-react";
+import React, { Component } from "react";
+import { AgGridReact } from "ag-grid-react";
 import Input from './Input';
 
 export default class extends Component {
     constructor(props) {
         super(props);
         this.onPriceChange = this.onPriceChange.bind(this);
+        this.onBtnClick = this.onBtnClick.bind(this);
         this.state = {
             columnDefs: this.createColumnDefs(),
-            rowData: this.createRowData()
-        }
+            rowData: this.createRowData(),
+            sidebarOpen: true
+        };
     }
 
     onGridReady(params) {
@@ -18,23 +20,28 @@ export default class extends Component {
 
         this.gridApi.sizeColumnsToFit();
     }
-    onPriceChange(){
+    onBtnClick(){
+        this.setState({
+            sidebarOpen: !this.state.sidebarOpen
+        });
+    }
+    onPriceChange() {
         console.log('on price change');
     }
     createColumnDefs() {
         return [
-            {headerName: "Make", field: "make"},
-            {headerName: "Model", field: "model"},
+            { headerName: "Make", field: "make" },
+            { headerName: "Model", field: "model" },
             {
-                headerName: "Price", 
+                headerName: "Price",
                 field: "price",
                 cellRendererFramework: (params) => {
                     const cellData = params.data;
                     const optInElement = (
                         <Input
-                        type="text"
-                        onChange={() => this.onPriceChange()}
-                        value={cellData}
+                            type="text"
+                            onChange={() => this.onPriceChange()}
+                            value={cellData}
                         />);
                     return optInElement;
                 }
@@ -44,29 +51,70 @@ export default class extends Component {
 
     createRowData() {
         return [
-            {make: "Toyota", model: "Celica", price: 35000},
-            {make: "Ford", model: "Mondeo", price: 32000},
-            {make: "Porsche", model: "Boxter", price: 72000}
+            { make: "Toyota", model: "Celica", price: 35000 },
+            { make: "Ford", model: "Mondeo", price: 32000 },
+            { make: "Porsche", model: "Boxter", price: 72000 }
         ];
+    }
+
+    componentWillUpdate(nextProps, nextState){
+        const api = this.gridApi;
+        if (this.state.sidebarOpen !== nextState.sidebarOpen && api) {
+            api.sizeColumnsToFit();
+        }
     }
 
     render() {
         let containerStyle = {
-            height: 115,
+            height: '100%',
             width: 500
+        };
+
+        let contentWrapperStyle = {
+            height: '100%',
+            width: '100%'
+        };
+
+        let sidebarStyle = {
+            display: 'inline-block',
+            width: '350px',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            left: this.state.sidebarOpen? '0' : '-350px'
+        };
+
+        let gridWrapperStyle = {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            left: this.state.sidebarOpen? '350px' : '0'
         };
 
         return (
             <div style={containerStyle} className="ag-fresh">
                 <h1>Simple ag-Grid React Example</h1>
-                <AgGridReact
-                    // properties
-                    columnDefs={this.state.columnDefs}
-                    rowData={this.state.rowData}
-                    suppressRowClickSelection
-                    // events
-                    onGridReady={this.onGridReady}>
-                </AgGridReact>
+                <div id="content-wrapper" style={contentWrapperStyle}>
+                    <div id="sidebar" style={sidebarStyle}>
+                        This is sidebar
+                    </div>
+                    <div id="grid-wrapper" style={gridWrapperStyle}>
+                        <div>
+                            <button onClick={this.onBtnClick}>Toggle Sidebar </button>
+                        </div>
+                        <AgGridReact
+                            // properties
+                            onGridReady={this.onGridReady}
+                            columnDefs={this.state.columnDefs}
+                            rowData={this.state.rowData}
+                            suppressRowClickSelection>
+                        </AgGridReact>
+                    </div>
+                </div>
             </div>
         )
     }
